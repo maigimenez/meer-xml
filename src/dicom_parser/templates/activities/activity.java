613a03aa746612a25e032cdf284356f91fcd01a3
Mai Giménez <la.reina.roja@gmail.com>
2013-07-19 13:50:04 +0200

    Added a toast to get selected item and a first intent to change from one activity to another

--- src/dicom_parser/templates/activities/activity.java
+++ src/dicom_parser/templates/activities/activity.java
@@ -1,9 +1,14 @@
 package {{ package_name }};
 
 import android.os.Bundle;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.app.Activity;
+import android.widget.Toast;
+import android.view.View;
+import android.content.Intent;
 
 public class {{ activity_name }} extends Activity {
 
--- src/dicom_parser/templates/activities/listview.java
+++ src/dicom_parser/templates/activities/listview.java
@@ -1,5 +1,14 @@
 
-        ListView listview_{{string_array}} = (ListView) findViewById(R.id.list_{{ string_array }});
+        ListView listview_{{ string_array }} = (ListView) findViewById(R.id.list_{{ string_array }});
         String[] items = getResources().getStringArray(R.array.list_{{string_array}});
         ArrayAdapter<String> adapter_{{string_array}} = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
         listview_{{string_array}}.setAdapter(adapter_{{string_array}});
+
+        listview_{{ string_array }}.setOnItemClickListener(new OnItemClickListener() {
+			public void onItemClick(AdapterView<?> adapterview, View view, int position, long id) {
+				Toast.makeText(getApplicationContext(), 
+						"Click ListItem Number " + position, Toast.LENGTH_LONG).show();
+				Intent i = new Intent(getBaseContext(),Edit_radlex_rid29896_radlex_rid34261.class);
+				startActivity(i);			
+			}
+		});
